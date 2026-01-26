38b934b3c439e323b1ce52dc84cfb1648b0f27c6
knowsyash <codeknowsyash@gmail.com>
2026-01-27 00:42:50 +0530

    Fix missing API_URL import in ResetPasswordPage

--- frontend/src/pages/ResetPasswordPage/ResetPasswordPage.jsx
+++ frontend/src/pages/ResetPasswordPage/ResetPasswordPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useSearchParams, useNavigate } from "react-router-dom";
+import API_URL from "../../config";
 import "./ResetPasswordPage.css";
 
 const ResetPasswordPage = () => {
